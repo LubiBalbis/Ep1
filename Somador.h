3a6308032a01d1ad7dd2959a4c070fb4cdb09f2c
LubiBalbis <148012621+LubiBalbis@users.noreply.github.com>
2023-10-16 18:28:58 -0300

    Create Somador.h

---
+++ Somador.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include "Sinal.h"
+
+
+class Somador {
+    private:
+    
+    public:
+    Somador();
+    virtual ~Somador();
+    Sinal* processar(Sinal* sinalIN1, Sinal* sinalIN2);
+};
